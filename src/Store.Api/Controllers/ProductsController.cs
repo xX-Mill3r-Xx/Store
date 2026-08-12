f607ae003bae0490b328c30577cfc870358540de
Miller M. Vasco <millermartinsvasco1@gmail.com>
2026-08-12 17:42:01 -0300

    ✨ feat: testando a api

---
+++ src/Store.Api/Controllers/ProductsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Store.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/products")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok("Teste da minha API");
+        }
+    }
+}
