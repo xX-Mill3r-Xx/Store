f0591f7e6cd7b3e0747512b2a1b3bfb612e06bfe
Miller M. Vasco <millermartinsvasco1@gmail.com>
2026-08-12 21:15:08 -0300

    ✨ feat: ajustando o codigo

---
+++ src/Store.Api/Dtos/CreateProductRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Store.Api.Dtos
+{
+    public class CreateProductRequest
+    {
+        [Required]
+        [StringLength(100)]
+        public string Nmar {  get; set; } = string.Empty;
+
+        [Range(0.01, double.MaxValue)]
+        public decimal Price { get; set; }
+    }
+}
---
+++ src/Store.Api/Dtos/ProductResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Store.Api.Dtos
+{
+    public class ProductResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
+}
