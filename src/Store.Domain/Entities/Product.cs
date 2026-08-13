f38f9171371465c60ded8b3d305ed9b7e986e1a8
Miller M. Vasco <millermartinsvasco1@gmail.com>
2026-08-13 18:14:05 -0300

    🐛 : fix/ corrigindo as referencias

---
+++ src/Store.Domain/Entities/Product.cs
@@ -0,0 +1,30 @@
+﻿namespace Store.Domain.Entities
+{
+    public sealed class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public Product(int id, string name, decimal price)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException(
+                    "O nome do produto é obrigatório.",
+                    nameof(name));
+            }
+
+            if (price <= 0)
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(price),
+                        "O preço do produto deve ser maior que zero.");
+            }
+
+            Id = id;
+            Name = name.Trim();
+            Price = price;
+        }
+    }
+}
